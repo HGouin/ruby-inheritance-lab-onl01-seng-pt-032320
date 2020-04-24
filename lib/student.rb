8d7b71e859e2804d16a8fb477a0be7de46de7427
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-24 06:26:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "./user.rb"
 class Student
 attr_accessor :first_name, :last_name
 
-def initialize(first_name, last_name)
+def initialize
   @first_name = first_name
   @last_name = last_name
 end
